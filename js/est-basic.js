64015decdaec6e41500454080012c56b71c0cbc6
vwannev <32233459+vwannev@users.noreply.github.com>
2025-03-31 14:35:17 +0900

    Update est-basic.js

--- js/est-basic.js
+++ js/est-basic.js
@@ -1,3 +1,18 @@
+document.documentElement.addEventListener('touchstart', function (event) {
+     if (event.touches.length > 1) {
+          event.preventDefault(); 
+        } 
+    }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+     var now = (new Date()).getTime();
+     if (now - lastTouchEnd <= 300) {
+          event.preventDefault(); 
+        } lastTouchEnd = now; 
+    }, false);
+
 document.addEventListener("DOMContentLoaded", function () {
     const selectBoxes = document.querySelectorAll(".select-box");
 
